93fb2594a353d45912ee3093dbfc1f08c46f56a7
gopalsays <gkkg738@gmail.com>
2021-08-11 18:14:08 +0530

    initial commit

---
+++ SampleApp1/app/src/main/java/com/gopal/devjunction/sampleapp1/registration/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.gopal.devjunction.sampleapp1.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gopal.devjunction.sampleapp1.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+    }
+}
